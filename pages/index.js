ba88382353e2002ef2d0af1fd39f72ac6b58fb9b
Magesh_Leo <52460267+Magesh-Leo@users.noreply.github.com>
2022-04-14 18:47:31 +0530

    add some content in home page

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,10 @@ export default function Home() {
   return (
     <div>
       <Navbar/>
+      <div className=' flex flex-col justify-center items-center w-full mt-60'>
+        <h1 className='font-bold items-center text-4xl'>Want To Build Your Portfolio</h1>
+        <br/><span className='text-green-500'>Click to <strong>Create new portfolio</strong> Button</span>
+      </div>
     </div>
   )
 }
