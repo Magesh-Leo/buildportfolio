56a3853209c28ac13e508d116e7852644906e5de
Magesh_Leo <52460267+Magesh-Leo@users.noreply.github.com>
2022-04-20 15:14:11 +0530

    state name duplicate delete

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -235,7 +235,7 @@ const Create = () =>  {
                         State
                     </label>
                     <div className="relative">
-                        <select name='state' value={state} onChange={(e)=>setState(e.target.value)} className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="state">
+                        <select name='state' value={state} onChange={(e)=>setState(e.target.value)} className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
                             <option value="Tamilnadu">Tamil Nadu</option>
                             <option value='Kerala'>Kerala</option>
                             <option value='Andrapradhesh'>Andra pradesh</option>
