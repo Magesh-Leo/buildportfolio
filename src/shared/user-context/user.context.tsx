1bf6681272d1f1d39d40669b861c3f8ad3920676
Magesh_Leo <52460267+Magesh-Leo@users.noreply.github.com>
2022-04-20 20:03:31 +0530

    clear bug

--- src/shared/user-context/user.context.tsx
+++ src/shared/user-context/user.context.tsx
@@ -1,10 +1,10 @@
-import router from 'next/router';
-import React, { useReducer, useContext, createContext, useEffect } from 'react';
+import router from "next/router";
+import React, { useReducer, useContext, createContext, useEffect } from "react";
 
-import tokenService from '../token.service';
-import { isObjNotEmpty } from '../utils/typeGaurds';
-import { User } from './User';
-import { UserActions, UserActionTypes } from './userContext.types';
+import tokenService from "../token.service";
+import { isObjNotEmpty } from "../utils/typeGaurds";
+import { User } from "./User";
+import { UserActions, UserActionTypes } from "./userContext.types";
 
 type UserStateContextType = {
   userState: UserState;
@@ -24,45 +24,44 @@ const userReducer = (state: UserState, action: UserActions): any => {
   switch (action.type) {
     case UserActionTypes.ADD_USER: {
       return { ...state, user: action.payload };
-    } 
+    }
     case UserActionTypes.REMOVE_USER: {
       return { user: {} };
     }
     default: {
-      throw new Error(`Unhandled action type: ${action.type}`);
+      throw new Error(`Unhandled action type: ${action}`);
     }
   }
 };
 
 function UserProvider({ children }: UserProviderProps) {
   const [state, dispatch] = useReducer(userReducer, { user: {} });
-  const { getUser, setUser,removeUser:removeUserLS } = tokenService;
+  const { getUser, setUser, removeUser: removeUserLS } = tokenService;
   const user = getUser();
 
   const reloadUser = () => {
-
     if (isObjNotEmpty(user)) {
-      console.log(user, 'dispatching, 🟢🟢🟢');
+      console.log(user, "dispatching, 🟢🟢🟢");
       dispatch({
         type: UserActionTypes.ADD_USER,
         payload: user,
       });
     } else {
       // Route user to login page
-      console.log('🔴', user, 'User not found', '🔴');
-       // Route user to login page
-       const unprotectedRoutes = ['/sign-in'];
-       const isUnprotectedRoute = unprotectedRoutes.some((route) =>
-         router.pathname.includes(route)
-       );
-       if (!isUnprotectedRoute) {
-         router.push('/sign-in');
-       }
+      console.log("🔴", user, "User not found", "🔴");
+      // Route user to login page
+      const unprotectedRoutes = ["/sign-in"];
+      const isUnprotectedRoute = unprotectedRoutes.some((route) =>
+        router.pathname.includes(route)
+      );
+      if (!isUnprotectedRoute) {
+        router.push("/sign-in");
+      }
     }
   };
 
   const addUser = (user: User): void => {
-    console.log('🟢', user, 'adding user', '🟢');
+    console.log("🟢", user, "addingu user", "🟢");
     // Set user in local storage
     setUser(user);
     dispatch({
@@ -76,7 +75,6 @@ function UserProvider({ children }: UserProviderProps) {
       type: UserActionTypes.REMOVE_USER,
     });
     removeUserLS();
-
   };
 
   useEffect(() => {
@@ -95,7 +93,7 @@ function UserProvider({ children }: UserProviderProps) {
 function useUser() {
   const context = useContext(UserStateContext);
   if (context === undefined) {
-    throw new Error('useUser must be used within a UserProvider');
+    throw new Error("useUser must be used within a UserProvider");
   }
   return context;
 }
